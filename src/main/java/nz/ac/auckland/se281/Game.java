ea9302b285355663cbe34ba3ac678e84d8ea9aea
Tyler <young.h.tyler@gmail.com>
2024-05-03 20:41:38 +1200

    Added logic for printing the number of rounds and asking for input.

--- src/main/java/nz/ac/auckland/se281/Game.java
+++ src/main/java/nz/ac/auckland/se281/Game.java
@@ -1,16 +1,27 @@
 package nz.ac.auckland.se281;
 
+import java.util.Scanner; // Import the Scanner class.
 import nz.ac.auckland.se281.Main.Choice;
 import nz.ac.auckland.se281.Main.Difficulty;
 
 /** This class represents the Game is the main entry point. */
 public class Game {
 
+  public int rounds;
+
   public void newGame(Difficulty difficulty, Choice choice, String[] options) {
-    // the first element of options[0]; is the name of the player
+    rounds = 0;
+
+    MessageCli.WELCOME_PLAYER.printMessage(options[0]);
   }
 
-  public void play() {}
+  public void play() {
+    rounds += 1;
+    MessageCli.START_ROUND.printMessage(String.valueOf(rounds));
+
+    Scanner scanner = new Scanner(System.in);
+    MessageCli.ASK_INPUT.printMessage();
+  }
 
   public void endGame() {}
 
